b623dc692e6b599de859e2676d645baa8d04b38a
Jonas Almeida <jalmeida@mathbiol.org>
2017-07-10 15:55:35 -0400

    all is good, time for the .getJSON method

--- yext.js
+++ yext.js
@@ -48,3 +48,35 @@ if(yext.div0){
         yext.callResponse=x
     }
 }
+
+yext.getJSON=function(url){ // getJSON with promises
+    url = 'https://script.google.com/macros/s/AKfycbxH_t0MnnzTDvWnGKwpyIJUkJJqpuBOiZjwnerTgtGLsONojZg/exec?url='+encodeURIComponent(url)
+
+
+}
+
+yext.jobs={}
+
+yext.getJSON=function(url){ // getJSON with promises
+    url = 'https://script.google.com/macros/s/AKfycbxH_t0MnnzTDvWnGKwpyIJUkJJqpuBOiZjwnerTgtGLsONojZg/exec?url='+encodeURIComponent(url)
+    return new Promise(function(resolve, reject) {
+        var s = document.createElement('script')
+        var uid = "job"+Date.now().toString()+Math.random().toString().slice(2)
+        s.id=uid
+        s.src=url+'&callback=yext.jobs.'+uid
+        yext.jobs[uid]=function(x){
+            resolve(x)
+            setTimeout(function(){  // remove the scripts that were successfull 
+                delete yext.jobs[uid]
+                var s = document.getElementById(uid)
+                s.parentElement.removeChild(s)
+            },1000)
+        }
+        s.onerror=function(){
+            reject({"error":('error loading '+url)})
+        }
+        document.head.appendChild(s)
+    })
+}
+
+
